493a6aaf7857c16d8e42f814058f3579f8173050
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-03-19 15:17:42 -0500

    Preparing for merge

--- TabloidCLI/UserInterfaceManagers/AuthorDetailManager.cs
+++ TabloidCLI/UserInterfaceManagers/AuthorDetailManager.cs
@@ -24,6 +24,7 @@ namespace TabloidCLI.UserInterfaceManagers
 
         public IUserInterfaceManager Execute()
         {
+            Console.Clear();
             Author author = _authorRepository.Get(_authorId);
             Console.WriteLine($"{author.FullName} Details");
             Console.WriteLine(" 1) View");
