3c50b811125183e2d3877a9be09ae4974d11e587
anna-moore <ammoore434@gmail.com>
2021-03-19 13:24:15 -0500

    Journals GetAll

--- TabloidCLI/Repositories/JournalRepository.cs
+++ TabloidCLI/Repositories/JournalRepository.cs
@@ -1,10 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
 
-namespace TabloidCLI.Repositories
+namespace TabloidCLI
 {
-    class JournalRepository
+    class JournalRepository : DatabaseConnector, IRepository<Journal>
     {
+        public JournalRepository(string connectionString) : base(connectionString) { }
+
+        public List<Journal> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, 
+                                               Title, 
+                                              Content, 
+                                              CreateDateTime
+                                       From Journal";
+
+                    List<Journal> journals = new List<Journal>();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Journal journal = new Journal()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            Content = reader.GetString(reader.GetOrdinal("Content")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime")),
+                        };
+
+                        journals.Add(journal);
+
+                    }
+                    reader.Close();
+
+                    return journals;
+                }
+            }
+        }
     }
 }
