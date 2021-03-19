74d311915a9dd7097910185834c58760508b79d4
anna-moore <ammoore434@gmail.com>
2021-03-19 13:05:44 -0500

    journal class

--- TabloidCLI/Models/Journal.cs
+++ TabloidCLI/Models/Journal.cs
@@ -6,5 +6,13 @@ namespace TabloidCLI.Models
 {
     class Journal
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreateDateTime { get; set; }
+
     }
 }
