60ca4f4210ff9fe156af33b28e795bd7973bb160
anna-moore <ammoore434@gmail.com>
2021-03-22 13:29:10 -0500

    background color

--- TabloidCLI/UserInterfaceManagers/BackgroundColor.cs
+++ TabloidCLI/UserInterfaceManagers/BackgroundColor.cs
@@ -4,10 +4,14 @@ using System.Text;
 
 namespace TabloidCLI.UserInterfaceManagers
 {
-    class BackgroundColor
+    class BackgroundColor : IUserInterfaceManager
     {
-        //doesnt need IUserInterfaceManager????
-        public void  Execute()
+        private  IUserInterfaceManager _parentUI;
+        public BackgroundColor(IUserInterfaceManager parentUI)
+        {
+            _parentUI = parentUI;
+        }
+        public IUserInterfaceManager Execute()
         {
 
             Console.WriteLine("Color Menu");
@@ -16,12 +20,14 @@ namespace TabloidCLI.UserInterfaceManagers
             Console.WriteLine(" 3) Dark Green");
             Console.WriteLine(" 4) Dark Cyan");
             Console.WriteLine(" 5) Dark Red");
-            Console.WriteLine(" 6) Magenta");
+            Console.WriteLine(" 6) Dark Magenta");
             Console.WriteLine(" 7) Dark Yellow");
             Console.WriteLine(" 8) Gray");
             Console.WriteLine(" 9) Blue");
             Console.WriteLine(" 10) Green");
+            Console.WriteLine(" 11) Reset");
             Console.WriteLine(" 0) Return to Main Menu");
+
 
             Console.Write("> ");
             string choice = Console.ReadLine();
@@ -30,28 +36,48 @@ namespace TabloidCLI.UserInterfaceManagers
             {
                 case "1":
                     Console.BackgroundColor = ConsoleColor.Red;
+                    Console.Clear();
                     return this;
                 case "2":
                     Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    Console.Clear();
                     return this;
                 case "3":
                     Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.Clear();
                     return this;
                 case "4":
                     Console.BackgroundColor = ConsoleColor.DarkCyan;
+                    Console.Clear();
                     return this;
                 case "5":
                     Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.Clear();
+                    return this;
                 case "6":
                     Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                    Console.Clear();
+                    return this;
                 case "7":
                     Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    Console.Clear();
+                    return this;
                 case "8":
-                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    Console.Clear();
+                    return this;
                 case "9":
                     Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.Clear();
+                    return this;
                 case "10":
                     Console.BackgroundColor = ConsoleColor.Green;
+                    Console.Clear();
+                    return this;
+                case "11":
+                    Console.ResetColor();
+                    Console.Clear();
+                    return this;
 
                 case "0":
                     return _parentUI;
--- TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
+++ TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
@@ -31,7 +31,7 @@ namespace TabloidCLI.UserInterfaceManagers
                 case "4": return new PostManager(this, CONNECTION_STRING);
                 case "5": return new TagManager(this, CONNECTION_STRING);
                 case "6": return new SearchManager(this, CONNECTION_STRING);
-                case "7": return new BackgroundColor(this, CONNECTION_STRING);
+                case "7": return new BackgroundColor(this);
                 case "0":
                     Console.WriteLine("Good bye");
                     return null;
