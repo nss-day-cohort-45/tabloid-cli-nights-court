9ef85692376620e2f3da39e731dd54b9ac9b6b90
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-03-22 15:31:02 -0500

    Added Note Model

---
+++ TabloidCLI/Models/Note.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace TabloidCLI.Models
+{
+    class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime NoteDate { get; set; }
+        public int PostId { get; set; }
+        public Note()
+        {
+            NoteDate = DateTime.Now;
+        }
+        public override string ToString()
+        {
+            return $"{Title} ({Content} {NoteDate})";
+        }
+    }
+}
